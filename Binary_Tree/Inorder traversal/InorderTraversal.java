cf387df652164e42a6d18aa3093883c8fd8f71cc
iamkishlayverma <verma.kishlay688@gmail.com>
2021-05-16 11:15:07 +0530

    Added Binary Search folder with solution for inorder traversal in it

---
+++ Binary_Tree/Inorder traversal/InorderTraversal.java
@@ -0,0 +1,42 @@
+/*
+Inorder Traversal:
+
+Algorithm Inorder(tree)
+   1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+   2. Visit the root.
+   3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+Uses of Inorder
+In case of binary search trees (BST), Inorder traversal gives nodes in non-decreasing order. To get nodes of BST in non-increasing order, a variation of Inorder traversal where Inorder traversal s reversed can be used.
+Example: Inorder traversal for the above-given figure is 4 2 5 1 3.
+* */
+
+package com.company;
+
+class Node {
+    int key;
+    Node left, right;
+    Node(int key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class InorderTraversal {
+    public static void inorder(Node root) {
+        if (root == null)
+            return;
+        inorder(root.left);
+        System.out.print(root.key + " ");
+        inorder(root.right);
+    }
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        inorder(root);
+    }
+}
